76b13abc569f85cd85ee1e9861e620fc2f811f84
Phan M. Hung <pmhungpro1@gmail.com>
2021-11-09 00:48:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 function display(n) {
-  if (n==1) {
-    var elem= document.getElementById("sub1")
+    var elem = document.getElementById("sub1")
     elem.style.display = "block"
     var noti = document.getElementById("sub")
     noti.style.display = "block"
-  }
-  else if (n==2) {
-    var elem = document.getElementById("sub2")
-    elem.style.display = "block"
-    var noti = document.getElementById("sub")
-    noti.style.display = "block"
-  }
-  else {
-    var elem = document.getElementById("sub3")
-    elem.style.display = "block"
-    var noti = document.getElementById("sub")
-    noti.style.display = "block"
-  }
+    var headersub1= document.getElementById("hsub")
+    headersub1.innerHTML = "Новость "
+    headersub1.innerHTML += n
+    headersub1.style.fontWeight= "bold"
+    headersub1.style.fontSize= "35px"
+    var h4sub1= document.getElementById("h4sub")
+    h4sub1.innerHTML = "Новость "
+    h4sub1.innerHTML += n
+    h4sub1.innerHTML += ": Lorem ipsum dolor sit amet consectetur adipisicing elit. A, alias."
+    h4sub1.style.fontWeight= "normal"
+    h4sub1.style.marginBottom= "0px"
 }
 function destroy() {
   var elem1= document.getElementById("sub1")
   elem1.style.display = "none"
-  var elem2= document.getElementById("sub2")
-  elem2.style.display = "none"
-  var elem3= document.getElementById("sub3")
-  elem3.style.display = "none"
   var noti = document.getElementById("sub")
   noti.style.display = "none"
 }
